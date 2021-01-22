fdca0e9fb7ba794db972ac9d50e53fd5c48b37f6
gurgenk022 <gurgenk022@gmail.com>
2021-01-22 15:17:29 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("New customer is Id: "+customer.Id);
+            Console.WriteLine("New customer is name: "+customer.Name);
+            Console.WriteLine("New customer is surname: "+customer.Surname);
+
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Delete customer is Id: "+customer.Id);
+            Console.WriteLine("Delete customer is name: "+customer.Name);
+            Console.WriteLine("Delete customer is surname: "+customer.Surname);
+        }
+        public void ListCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Id+" "+customer.Name+" "+customer.Surname);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 12345678;
+            customer1.Name = "Ahmet";
+            customer1.Surname = "YILDIZ";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 12345687;
+            customer2.Name = "Buse";
+            customer2.Surname = "YILDIZ";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            foreach (var c in customers)
+            {
+                Console.WriteLine(c.Id);
+                Console.WriteLine(c.Name);
+                Console.WriteLine(c.Surname);
+            }
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+
+            customerManager.DeleteCustomer(customer1);
+            customerManager.DeleteCustomer(customer2);
+
+            customerManager.ListCustomer(customer1);
+            customerManager.ListCustomer(customer2);
+        }
+    }
+}
